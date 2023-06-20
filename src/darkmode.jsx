c678051c6c4ba1bd4ea5a6babef88a5fe6dd138d
jasper <jasper.mittag@outlook.de>
2023-06-21 01:41:45 +0200

    darkmode

--- src/darkmode.jsx
+++ src/darkmode.jsx
@@ -1,28 +1,41 @@
-const lightMode = document.querySelector('.sun');
-const darkMode = document.querySelector('.moon');
+import React from 'react'
 
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-lightMode.onclick = function(){
-  document.documentElement.setAttribute('data-theme', 'dark');
-  lightMode.style = "display: none";
-  darkMode.style = "display: block";
-  localStorage.setItem('theme', 'dark');
-}
 
-darkMode.onclick = function(){
-  document.documentElement.setAttribute('data-theme', 'light');
-  lightMode.style = "display: block";
-  darkMode.style = "display: none";
-  localStorage.setItem('theme', 'light');
-  
-}
+function darkmode() {
+    const lightMode = document.querySelector('.sun');
+    const darkMode = document.querySelector('.moon');
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-    if (currentTheme === 'dark') {
-        lightMode.style = "display: none";
-      darkMode.style = "display: block";
+    console.log(lightMode, darkMode);
+
+    lightMode.onclick = () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    lightMode.style = "display: none";
+    darkMode.style = "display: block";
+    localStorage.setItem('theme', 'dark');
     }
+
+    darkMode.onclick = () => {
+    document.documentElement.setAttribute('data-theme', 'light');
+    lightMode.style = "display: block";
+    darkMode.style = "display: none";
+    localStorage.setItem('theme', 'light');
+    
+    }
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            lightMode.style = "display: none";
+        darkMode.style = "display: block";
+        }
+    }
+
+
+  return null
 }
+
+export default darkmode
